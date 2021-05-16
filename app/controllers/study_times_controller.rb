ddb67de47f2abf34bb6f6a73d445ba19054e238d
EC2 Default User <ec2-user@ip-172-31-15-56.ap-northeast-1.compute.internal>
2021-05-16 06:17:40 +0000

    [create]studytimesコントローラの作成

---
+++ app/controllers/study_times_controller.rb
@@ -0,0 +1,16 @@
+class StudyTimesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def updated_at
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'study_times/new'
+  get 'study_times/create'
+  get 'study_times/index'
+  get 'study_times/edit'
   root to: 'homes#top'
   get 'homes/about'
   devise_for :users
