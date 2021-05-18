0bf177f7d9e06a71d8d0508e642cad8a1f7b92c0
EC2 Default User <ec2-user@ip-172-31-15-56.ap-northeast-1.compute.internal>
2021-05-18 11:26:49 +0000

    [create]いいね機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,15 @@
 class FavoritesController < ApplicationController
-  def new
+  def create
+    recommend = Recommend.find(params[:recommend_id])
+    favorite = current_user.favorites.new(recommend_id: recommend.id)
+    favorite.save
+    redirect_to recommend_path(recommend)
   end
 
   def destroy
+    recommend = Recommend.find(params[:recommend_id])
+    favorite = current_user.favorites.find_by(recommend_id: recommend.id)
+    favorite.destroy
+    redirect_to recommend_path(recommend)
   end
 end
