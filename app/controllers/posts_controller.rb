31e7a202756657470e46259ceb653890804d6ac7
EC2 Default User <ec2-user@ip-172-31-15-56.ap-northeast-1.compute.internal>
2021-06-10 12:07:49 +0000

    [update]ひとりごと削除後のリダイレクト先の変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました。"
-    redirect_to posts_path
+    redirect_to user_path(current_user)
   end
 
   def index
